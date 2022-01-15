f43baa8b220f9600752173c8e4ba746d3aa5490c
Lightling <contactlightling@gmail.com>
2022-01-15 15:12:30 -0500

    add more Mesh code

--- Mesh.h
+++ Mesh.h
@@ -7,20 +7,22 @@
 class Mesh
 {
 private:
-	Microsoft::WRL::ComPtr<ID3D11Buffer>  bufferVertex;
-	Microsoft::WRL::ComPtr<ID3D11Buffer>  bufferIndex;
+	Microsoft::WRL::ComPtr<ID3D11Buffer>            bufferVertex;
+	Microsoft::WRL::ComPtr<ID3D11Buffer>            bufferIndex;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext>     deviceContext;
+	int                                             countIndex;
 public:
 	Mesh(
-		Vertex*       _vertices,
-		int           _vertexCount,
-		unsigned int* _indices,
-		int           _indexCount,
+		Vertex*                                     _vertices,
+		int                                         _vertexCount,
+		unsigned int*                               _indices,
+		int                                         _indexCount,
 		Microsoft::WRL::ComPtr<ID3D11Device>        _device,
 		Microsoft::WRL::ComPtr<ID3D11DeviceContext> _context);
 	~Mesh();
 
-	void                                  Draw();
-	Microsoft::WRL::ComPtr<ID3D11Buffer>* GetVertexBuffer();
-	Microsoft::WRL::ComPtr<ID3D11Buffer>* GetIndexBuffer();
-	int                                   GetIndexCount();
+	void                                            Draw();
+	Microsoft::WRL::ComPtr<ID3D11Buffer>*           GetVertexBuffer();
+	Microsoft::WRL::ComPtr<ID3D11Buffer>*           GetIndexBuffer();
+	int                                             GetIndexCount();
 };
