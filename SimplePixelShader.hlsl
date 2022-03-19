a4bfd4e0c38f95c879c5f5569318eca567c31a27
Lightling <contactlightling@gmail.com>
2022-03-19 16:49:03 -0400

    add specular

--- SimplePixelShader.hlsl
+++ SimplePixelShader.hlsl
@@ -15,7 +15,18 @@ float4 main(VertexToPixel input) : SV_TARGET
 	float3 ambientTint = ambient * tint;
 	float3 directionalLight1Dir = normalize(-directionalLight1.Direction);
 	float diffuse = saturate(dot(input.normal, directionalLight1Dir));
-	float3 final = (diffuse * directionalLight1.Color * tint) + ambientTint;
+
+	float3 view = normalize(cameraPosition - input.worldPosition);
+	float3 reflection = reflect(directionalLight1Dir, input.normal);
+
+	float specularExponent = (1.0f - roughness) * MAX_SPECULAR_EXPONENT;
+	float specular = 0;
+	if (specularExponent > 0.05f)
+	{
+		specular = pow(saturate(dot(reflection, view)), specularExponent);
+	}
+
+	float3 final = (diffuse * directionalLight1.Color * tint) + ambientTint + specular;
 
 	return float4(final, 1);
 }
