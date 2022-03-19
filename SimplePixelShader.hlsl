3d7c777db674245cfd768f6945c018909c0cfdaf
Lightling <contactlightling@gmail.com>
2022-03-19 18:12:50 -0400

    shader optimization: calculate view once per shader

--- SimplePixelShader.hlsl
+++ SimplePixelShader.hlsl
@@ -15,32 +15,32 @@ cbuffer ExternalData : register(b0)
 }
 
 // Gets the specular value for any light
-float calculateSpecular(float3 normal, float3 direction, float3 worldPosition, float3 cameraPosition, float roughness)
+float calculateSpecular(float3 normal, float3 direction, float3 view, float roughness)
 {
 	return getSpecular(
-		getView(cameraPosition, worldPosition),
+		view,
 		getReflection(direction, normal),
 		getSpecularExponent(roughness, MAX_SPECULAR_EXPONENT)
 	);
 }
 
 // Gets the RGB value of a pixel with a directional light
-float3 calculateDirectionalLight(Light light, float3 normal, float3 worldPosition, float3 cameraPosition, float roughness, float3 surfaceColor)
+float3 calculateDirectionalLight(Light light, float3 normal, float3 view, float roughness, float3 surfaceColor)
 {
 	float3 lightDirection = normalize(light.Direction);
 	float diffuse = getDiffuse(normal, -lightDirection);
-	float specular = calculateSpecular(normal, lightDirection, worldPosition, cameraPosition, roughness);
+	float specular = calculateSpecular(normal, lightDirection, view, roughness);
 
 	return (diffuse * surfaceColor + specular) * light.Intensity * light.Color;
 }
 
 // Gets the RGB value of a pixel with a point light
-float3 calculatePointLight(Light light, float3 normal, float3 worldPosition, float3 cameraPosition, float roughness, float3 surfaceColor)
+float3 calculatePointLight(Light light, float3 normal, float3 view, float3 worldPosition, float roughness, float3 surfaceColor)
 {
 	float3 lightDirection = normalize(worldPosition - light.Position);
 	float attenuation = getAttenuation(light.Position, worldPosition, light.Range);
 	float diffuse = getDiffuse(normal, -lightDirection);
-	float specular = calculateSpecular(normal, lightDirection, worldPosition, cameraPosition, roughness);
+	float specular = calculateSpecular(normal, lightDirection, view, roughness);
 
 	return (diffuse * surfaceColor + specular) * attenuation * light.Intensity * light.Color;
 }
@@ -48,7 +48,11 @@ float3 calculatePointLight(Light light, float3 normal, float3 worldPosition, flo
 // shader entry point
 float4 main(VertexToPixel input) : SV_TARGET
 {
+	// ensure input normals are normalized
 	input.normal = normalize(input.normal);
+
+	// view only needs calculated once, so pre-calculate here and pass it to lights
+	float3 view = getView(cameraPosition, input.worldPosition);
 
 	// start with ambient light and material tint
 	float3 light = ambient * tint;
@@ -59,10 +63,10 @@ float4 main(VertexToPixel input) : SV_TARGET
 		switch (lights[i].Type)
 		{
 		case LIGHT_TYPE_DIRECTIONAL:
-			light += calculateDirectionalLight(lights[i], input.normal, input.worldPosition, cameraPosition, roughness, tint);
+			light += calculateDirectionalLight(lights[i], input.normal, view, roughness, tint);
 			break;
 		case LIGHT_TYPE_POINT:
-			light += calculatePointLight(lights[i], input.normal, input.worldPosition, cameraPosition, roughness, tint);
+			light += calculatePointLight(lights[i], input.normal, view, input.worldPosition, roughness, tint);
 			break;
 		}
 	}
