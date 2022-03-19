6cbd6c54828df63c8409a6e96b55d1093d43f1ba
Lightling <contactlightling@gmail.com>
2022-03-19 14:18:08 -0400

    shader structs and helpers relocation to includes

--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -1,42 +1,11 @@
+#include "Includes.hlsli"
+
 cbuffer ExternalData : register(b0)
 {
 	matrix world;
 	matrix view;
 	matrix projection;
 }
-
-// Struct representing a single vertex worth of data
-// - This should match the vertex definition in our C++ code
-// - By "match", I mean the size, order and number of members
-// - The name of the struct itself is unimportant, but should be descriptive
-// - Each variable must have a semantic, which defines its usage
-struct VertexShaderInput
-{ 
-	// Data type
-	//  |
-	//  |   Name          Semantic
-	//  |    |                |
-	//  v    v                v
-	float3 localPosition	: POSITION;
-	float3 normal			: NORMAL;
-	float2 uv				: UV;
-};
-
-// Struct representing the data we're sending down the pipeline
-// - Should match our pixel shader's input (hence the name: Vertex to Pixel)
-// - At a minimum, we need a piece of data defined tagged as SV_POSITION
-// - The name of the struct itself is unimportant, but should be descriptive
-// - Each variable must have a semantic, which defines its usage
-struct VertexToPixel
-{
-	// Data type
-	//  |
-	//  |   Name          Semantic
-	//  |    |                |
-	//  v    v                v
-	float4 screenPosition	: SV_POSITION;
-	float2 uv				: TEXCOORD;
-};
 
 // --------------------------------------------------------
 // The entry point (main method) for our vertex shader
