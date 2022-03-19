e96c1c2e55e7905d930ad2efa5176e3214931e38
Lightling <contactlightling@gmail.com>
2022-03-19 17:09:27 -0400

    refactor into helpers

--- Helpers.hlsli
+++ Helpers.hlsli
@@ -1,6 +1,34 @@
 #ifndef __SHADER_HELPERS__
 #define __SHADER_HELPERS__
 
+float3 getView(float3 cameraPosition, float3 pixelWorldPosition)
+{
+	return normalize(cameraPosition - pixelWorldPosition);
+}
 
+float3 getReflection(float3 direction, float3 normal)
+{
+	return reflect(direction, normal);
+}
+
+float getSpecularExponent(float roughness, float max)
+{
+	return (1.0f - roughness) * max;
+}
+
+float getSpecular(float3 view, float3 reflection, float exponent)
+{
+	float specular = 0;
+	if (exponent > 0.05f)
+	{
+		specular = pow(saturate(dot(reflection, view)), exponent);
+	}
+	return specular;
+}
+
+float getDiffuse(float3 normal, float3 direction)
+{
+	return saturate(dot(normal, direction));
+}
 
 #endif
--- SimplePixelShader.hlsl
+++ SimplePixelShader.hlsl
@@ -1,4 +1,5 @@
 #include "Defines.hlsli"
+#include "Helpers.hlsli"
 #include "Lights.hlsli"
 
 cbuffer ExternalData : register(b0)
@@ -10,24 +11,24 @@ cbuffer ExternalData : register(b0)
 	Light directionalLight1;
 }
 
+float calculateSpecular(float3 normal, float3 worldPosition, float3 cameraPosition, float3 direction, float roughness)
+{
+	return getSpecular(
+		getView(cameraPosition, worldPosition),
+		getReflection(direction, normal),
+		getSpecularExponent(roughness, MAX_SPECULAR_EXPONENT)
+	);
+}
+
 float4 main(VertexToPixel input) : SV_TARGET
 {
 	input.normal = normalize(input.normal);
-	float3 ambientTint = ambient * tint;
 	float3 directionalLight1Dir = normalize(-directionalLight1.Direction);
-	float diffuse = saturate(dot(input.normal, directionalLight1Dir));
 
-	float3 view = normalize(cameraPosition - input.worldPosition);
-	float3 reflection = reflect(directionalLight1Dir, input.normal);
+	float diffuse = getDiffuse(input.normal, directionalLight1Dir);
+	float specular = calculateSpecular(input.normal, input.worldPosition, cameraPosition, directionalLight1Dir, roughness);
 
-	float specularExponent = (1.0f - roughness) * MAX_SPECULAR_EXPONENT;
-	float specular = 0;
-	if (specularExponent > 0.05f)
-	{
-		specular = pow(saturate(dot(reflection, view)), specularExponent);
-	}
-
-	float3 final = (diffuse * directionalLight1.Color * tint) + ambientTint + specular;
+	float3 final = (diffuse * directionalLight1.Color * tint) + (ambient * tint) + specular;
 
 	return float4(final, 1);
 }
